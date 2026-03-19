2345d21af8b6f7225f7131cbf39967d702b29e75
sherkishuerkishierikiri-spec <sherkishuerkishierikiri@gmail.com>
2026-03-19 07:17:24 -0600

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,18 @@
+input.onButtonPressed(Button.A, function() {
+    
+basic.showIcon(IconNames.Heart)
+
+input.onButtonPressed(Button.B, function() {
+    
+    basic.showIcon(IconNames.SmallHeart)
+
+input.onPinPressed(TouchPin.P2, function() {
+    
+    basic.showIcon(IconNames.Happy)
+
+input.onButtonPressed(Button.AB, function() {
+
+    basic.showIcon(IconNames.Yes)
+
+
+})})})})
